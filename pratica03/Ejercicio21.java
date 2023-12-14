073b1bfced9e2a1d8da1f0c5ebe007da136cac0d
Marcos <marcoscr7rojas@gmail.com>
2023-12-14 18:20:05 -0300

    Marcos 14/12/2023

--- pratica03/Ejercicio21.java
+++ pratica03/Ejercicio21.java
@@ -11,14 +11,13 @@ public class Ejercicio21 {
         //bucle 1
         for (tabla = 1; tabla < 11; tabla ++) {
           //salida
-          System.out.print("----------------------------------");
-          System.out.println("Que tabla que seas Mostrar?"); 
-          System.out.print("---------------------------------");
-
+          System.out.println("----------------------------------");
+          System.out.println("Que tabla que seas Mostrar?"+tabla); 
+          System.out.println("---------------------------------");
       //bucle 2
         for (int i = 1; i < 11; i++) {
       result= tabla * i;
-       System.out.println(tabla + "X"+i+ "="+ result);
+       System.out.println(tabla + "X"+ i + " = " + result);
    
      }
  
@@ -31,4 +30,4 @@ public class Ejercicio21 {
 
 
     }
-5
+
---
+++ pratica03/pratica3ejerciciow1.java
@@ -0,0 +1,18 @@
+package pratica03;
+/*
+ * @Marcos Rojas 
+ */
+public class pratica3ejerciciow1 {
+  
+  //bucle while
+    public static void main(String[] args) {
+     //variable
+     int i = 1 ;
+     //blucle
+     while (i < 11 ) {
+        System.out.println("Valor:"+i);
+     i++;
+    
+    }
+   } 
+}
