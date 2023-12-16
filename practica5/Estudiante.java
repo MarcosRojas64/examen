de148fb7d72cd2893b8d8bbfcce02797a34bf460
Marcos <marcoscr7rojas@gmail.com>
2023-12-16 18:41:36 -0300

    Marcos Rojas 16/12/2023

---
+++ practica5/Estudiante.java
@@ -0,0 +1,24 @@
+public class Estudiante extends persona {
+
+   private int creditos;
+
+public Estudiante(String nombre, int edad){
+
+  super(nombre,edad);
+   this.creditos = 60;
+
+}
+ public int getcreditos(){
+     return this.creditos;
+ }
+
+  public void mostrarDatos(){
+   super.mostrarDatos();
+   System.out.println("Estudiante Matriculado de " +creditos+ " Creditos");
+
+  }
+
+
+
+
+}
